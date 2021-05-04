b6448fd3e5d380ff78827568f1943e52d0243428
DDBull <dyussupov.d@gmail.com>
2021-05-04 15:49:35 +0500

    Medium. Greedy algorithm

---
+++ solutions/may-challenge/Non-decreasing Array/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public boolean checkPossibility(int[] nums) {
+
+        int count = 0;
+        int pos = -1;
+        int len = nums.length;
+
+        for (int i = 0; i + 1 < len; i++) {
+            if (nums[i] > nums[i + 1]) {
+                count++;
+                pos = i;
+            }
+        }
+
+        if (count > 1) {
+            return false;
+        }
+
+        if (count == 0) {
+            return true;
+        }
+
+        return (len < 3) || (pos == 0) || (pos + 1 >= len - 1)
+                || (nums[pos - 1] <= nums[pos + 1])
+                || (nums[pos] <= nums[pos + 2]);
+
+    }
+}
