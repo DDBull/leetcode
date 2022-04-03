264e487748b38900e06d1c253d8843b1b2415e32
Dauren Dyussupov <dyussupov.d@gmail.com>
2022-04-03 15:47:26 +0500

    Medium. Array

---
+++ solutions/31. Next Permutation/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+      int n = nums.size();
+      int l, r;
+
+      for (l = n - 2; l >= 0; l--) {
+        if (nums[l] < nums[l + 1]) break;
+      }
+
+      if (l < 0) reverse(nums.begin(), nums.end());
+      else {
+        for (r = n - 1; r > l; r--) {
+          if (nums[r] > nums[l]) break;
+        }
+
+        swap(nums[l], nums[r]);
+
+        reverse(nums.begin() + l + 1, nums.end());
+      }
+
+    }
+};
