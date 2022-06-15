c8b5c4e9940076894c7eea703a71e10588a33375
Dauren Dyussupov <dyussupov.d@gmail.com>
2022-06-15 17:23:28 +0500

    Medium. DP. Hash Table. LINQ

--- solutions/1048. Longest String Chain/Solution.cs
+++ solutions/1048. Longest String Chain/Solution.cs
@@ -54,4 +54,19 @@ public class Solution {
                return 0;
         }
     }
+
+    public int LongestStrChain(string[] words)
+    {
+        HashSet<string> wordsAsHashSet = words.ToHashSet();
+        Dictionary<string, int> dp = new();
+        return words.Max(GetLongestChain);
+
+        int GetLongestChain(string word)
+            => 1 + Enumerable.Range(0, word.Length)
+                    .Select(index => word[..index] + word[(index + 1)..])
+                    .Where(predecessor => wordsAsHashSet.Contains(predecessor))
+                    .Select(predecessor => dp.ContainsKey(predecessor) ? dp[predecessor] : dp[predecessor] = GetLongestChain(predecessor))
+                    .Append(0)
+                    .Max();
+    }
 }
