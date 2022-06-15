23d2a1890a7e0f62117334dc5d9e60c36cad103f
Dauren Dyussupov <dyussupov.d@gmail.com>
2022-06-15 22:06:30 +0500

    Medium. BFS

---
+++ solutions/542. 01 Matrix/Solution.cs
@@ -0,0 +1,47 @@
+public class Solution {
+    public int[][] UpdateMatrix(int[][] grid) {
+        Queue< Tuple <int, int> > q = new();
+        int n = grid.Length;
+        int m = grid[0].Length;
+        
+        int[] dx = new int[] { -1, 0, 0, 1 };
+        int[] dy = new int[] { 0, -1, 1, 0 };
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == 0)
+                {
+                    q.Enqueue(new Tuple <int, int> (i, j));
+                }
+            }
+        }
+        
+        while (q.Count > 0)
+        {
+            Tuple <int, int> cur = q.Dequeue();
+            int x = cur.Item1;
+            int y = cur.Item2;
+                        
+            for (int i = 0; i < 4; i++)
+            {
+                if (x + dx[i] >= 0 && x + dx[i] < n && y + dy[i] >= 0 && y + dy[i] < m && grid[x + dx[i]][y + dy[i]] == 1)
+                {
+                    grid[x + dx[i]][y + dy[i]] = grid[x][y] - 1;
+                    q.Enqueue( new Tuple<int, int> (x + dx[i], y + dy[i]));
+                }
+            }
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                grid[i][j] *= -1;
+            }
+        }
+        
+        return grid;
+    }
+}
