9fbea455797d3367892f12f491fb956bfe373b5c
Dauren Dyussupov <dyussupov.d@gmail.com>
2022-06-22 11:11:03 +0500

    Easy. Array

---
+++ solutions/350. Intersection of Two Arrays II/Solution.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] Intersect(int[] nums, int[] nums2) {
+        List<int> ans = new();
+        int[] count = new int[1001];
+        int[] count2 = new int[1001];
+        
+        foreach(int i in nums) count[i]++;
+        foreach(int i in nums2) count2[i]++;
+        
+        for (int i = 0; i <= 1000; i++)
+        {  
+            while (count[i]-- > 0 && count2[i]-- > 0) ans.Add(i);
+        }
+        
+        return ans.ToArray();
+    }
+}
